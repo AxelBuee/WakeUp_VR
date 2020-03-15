c3f388b23689ac46c0af8e77cd52c8c8161ca92d
Axel B <axel.buee@epita.fr>
2020-03-15 20:04:05 +0100

    Added scripts on flashlight, changed floor material

---
+++ Assets/Resources/Scripts/SwapMaterial.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapMaterial : MonoBehaviour
+{
+    public Material illuminated;
+    public Material off;
+
+    bool activated = false;
+
+    public void Swap()
+    {
+        Renderer rend = GetComponent<Renderer>();
+        if (rend != null)
+        {
+            if (activated)
+            {
+                activated = false;
+                rend.material = off;
+            }
+            else
+            {
+                rend.material = illuminated;
+                activated = true;
+            }
+        }
+    }
+}
