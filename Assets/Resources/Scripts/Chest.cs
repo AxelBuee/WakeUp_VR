c7a9618079c0e18873a72eb26ae87713a8b7b910
Pierre <pierre.perrier-ridet@edu.esiee.fr>
2020-05-12 06:01:55 +0200

    ajout coffre avec serrure sur scene à part

---
+++ Assets/Resources/Scripts/Chest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    public static int NbKeys;
+
+    void Awake() {
+        NbKeys = 0;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (NbKeys >= 3) {
+            this.gameObject.transform.GetChild(0).GetComponent<Rigidbody>().isKinematic = false;
+        }*/
+    }
+}
---
+++ Assets/Resources/Scripts/Locker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locker : MonoBehaviour
+{
+    /*public GameObject chest;
+    public bool canDestroyTheKey;
+    // Start is called before the first frame update
+
+    void Awake() {
+        canDestroyTheKey = false;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (canDestroyTheKey && other.gameObject.layer == 12) {      //si il a le layer "keyChest" et canDestroyTheKey
+            chest.GetComponent<Chest>().NbKeys += 1; //incrémenter le nombre de clefs sur le coffre (qui ont été insérées)
+            Destroy(other.gameObject);
+   
+        }
+    }*/
+
+    
+}
