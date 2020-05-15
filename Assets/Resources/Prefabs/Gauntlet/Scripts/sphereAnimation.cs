3c2418ba8ec7927799f5bb9e4e4cfb0ca99e4762
unknown <angelique.souvant@gmail.com>
2020-05-15 15:57:09 +0200

    sphere done

---
+++ Assets/Resources/Prefabs/Gauntlet/Scripts/sphereAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sphereAnimation : MonoBehaviour
+{
+    public GameObject sphereInGauntlet;
+    public GameObject sphereOut;
+    Animator m_Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void TriggerSphere()
+    {
+        MeshRenderer m = sphereInGauntlet.GetComponent<MeshRenderer>();
+        m_Animator = sphereInGauntlet.GetComponent<Animator>();
+        m.enabled = true;
+        sphereOut.SetActive(false);
+        m_Animator.SetTrigger("SphereDrop");
+    }
+}
